7bef706886169e399bafb20efa259251733996fa
Solidshots <seanbrent5@gmail.com>
2021-05-27 08:35:56 -0400

    home page and css styles

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,7 +6,16 @@ const {User, Post, Comment} = require('../models');
 //===============================================================
 
 router.get('/', (req, res) => {
-   res.render('home'); 
+   Post.findAll({
+      include: User
+   })
+   .then(dbPostData => {
+      //Serialize the db data into a new array
+      const posts = dbPostData.map(post => post.get({plain: true}));
+      //Render the array as an object to the homepage template
+      res.render('home', {posts});
+   })
+   .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
